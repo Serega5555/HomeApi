16e98833ac76264151f510076718d764bf1293aa
Serega5555 <lazarpalace@gmail.com>
2025-09-20 22:45:52 +0300

    Create UpdateRoomQuery.cs

---
+++ HomeApi.Data/Queries/UpdateRoomQuery.cs
@@ -0,0 +1,23 @@
+﻿using HomeApi.Contracts.Models.Rooms;
+
+namespace HomeApi.Data.Queries
+{
+    /// <summary>
+    /// Класс для передачи дополнительных параметров при обновлении комнаты
+    /// </summary>
+    public class UpdateRoomQuery
+    {
+        public string NewName { get; }
+        public int NewArea { get; }
+        public bool NewGasConnected { get; }
+        public int NewVoltage { get; }
+        
+        public UpdateRoomQuery(EditRoomRequest updateRoomQuery)
+        {
+            NewName = updateRoomQuery.NewName;
+            NewArea = updateRoomQuery.NewArea;
+            NewGasConnected = updateRoomQuery.NewGasConnected;
+            NewVoltage = updateRoomQuery.NewVoltage;
+        }
+    }
+}
