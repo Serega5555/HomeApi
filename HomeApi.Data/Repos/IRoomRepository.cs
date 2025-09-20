0bfe064ddfec65eb0571f6725852e785117e45a6
Serega5555 <lazarpalace@gmail.com>
2025-09-20 22:47:41 +0300

    Create IRoomRepository.cs

---
+++ HomeApi.Data/Repos/IRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using HomeApi.Data.Models;
+using HomeApi.Data.Queries;
+
+namespace HomeApi.Data.Repos
+{
+    /// <summary>
+    /// Интерфейс определяет методы для доступа к объектам типа Room в базе 
+    /// </summary>
+    public interface IRoomRepository
+    {
+        Task<Room> GetRoomById(string id);
+        Task<Room> GetRoomByName(string name);
+        Task<Room> GetRoomByNameExceptId(string id, string name);
+        Task AddRoom(Room room);
+        Task UpdateRoom(Room room, UpdateRoomQuery query);
+    }
+}
